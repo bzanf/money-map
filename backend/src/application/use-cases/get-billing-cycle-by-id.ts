18b0056cef98141a82d5bb46b7cb3770f9ce364b
bzanf <borysfreitas@gmail.com>
2024-10-28 22:01:21 -0300

    Create get-all billing cycle use-case and dto

---
+++ backend/src/application/use-cases/get-billing-cycle-by-id.ts
@@ -0,0 +1,10 @@
+import { BillingCycleRepository } from "../../domain/repositories/billing-cycle-repository";
+import { GetBillingCycleByIdDTO } from "../dtos/input/get-billing-cycle-by-id-dto";
+
+export class GetAllBillingCycles {
+    constructor(private billingCycleRepository: BillingCycleRepository) { }
+
+    async execute(input: GetBillingCycleByIdDTO) {
+        return await this.billingCycleRepository.findById(input.id);
+    }
+}
