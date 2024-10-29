915c1c44a3498abde29685d58ac379a394548097
bzanf <borysfreitas@gmail.com>
2024-10-29 16:27:44 -0300

    Implement the 'create' method of the mongodb billing cycle repository

--- backend/src/infrastructure/persistence/mongodb/repositories/mongo-billing-cycle-repository.ts
+++ backend/src/infrastructure/persistence/mongodb/repositories/mongo-billing-cycle-repository.ts
@@ -1,25 +1,32 @@
 import { BillingCycle } from "../../../../domain/entities/billing-cycle";
 import { BillingCycleRepository } from "../../../../domain/repositories/billing-cycle-repository";
-import { toDomainBillingCycle } from "../../../mapper/billing-cycle-mapper";
+import { toDocumentBillingCycle, toDomainBillingCycle } from "../../../mappers/billing-cycle-mapper";
 import { BillingCycleModel } from "../models/billing-cycle-model";
 
 export class MongoBillingCycleRepository implements BillingCycleRepository {
 
     async findAll(): Promise<BillingCycle[]> {
         const docs = await BillingCycleModel.find();
+
         return docs.length > 0
             ? docs.map(toDomainBillingCycle)
             : [];
     }
+
     findById(id: string): Promise<BillingCycle | null> {
         throw new Error("Method not implemented.");
     }
-    create(billingCycle: BillingCycle): Promise<BillingCycle> {
-        throw new Error("Method not implemented.");
+
+    async create(billingCycle: BillingCycle): Promise<BillingCycle> {
+        const billingCycleDocument = toDocumentBillingCycle(billingCycle);
+        const doc = await BillingCycleModel.create(billingCycleDocument);
+        return toDomainBillingCycle(doc);
     }
+
     update(billingCycle: BillingCycle): Promise<BillingCycle> {
         throw new Error("Method not implemented.");
     }
+
     delete(id: string): Promise<void> {
         throw new Error("Method not implemented.");
     }
