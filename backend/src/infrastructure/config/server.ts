b55da6fb9f29c324f44412a80a2c0e8e6d9a000c
bzanf <borysfreitas@gmail.com>
2024-10-30 19:37:47 -0300

    Add error handling

--- backend/src/infrastructure/config/server.ts
+++ backend/src/infrastructure/config/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import router from "../../interface/routes/router";
+import { errorHandler } from "../../interface/middlewares/errorHandler";
 const app = express();
 
 app.use(express.json());
 app.use("/api", router);
+app.use(errorHandler)
 
 const PORT = process.env.SERVER_PORT || 3000;
-
 export async function startServer() {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
