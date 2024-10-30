74045db67fb15c3489c514048db1e38bc7fbc6cd
bzanf <borysfreitas@gmail.com>
2024-10-30 19:39:53 -0300

    Update create billing and get billing by id use cases

--- backend/src/application/use-cases/create-billing-cycle.ts
+++ backend/src/application/use-cases/create-billing-cycle.ts
@@ -1,12 +1,14 @@
 import { BillingCycleRepository } from "../../domain/repositories/billing-cycle-repository";
 import { CreateBillingCycleDTO } from "../dtos/input/create-billing-cycle-dto";
-import { toDomainBillingCycle } from "../mappers/billing-cycle-mapper";
+import { BillingCycleResponseDTO } from "../dtos/output/billing-cycle-response-dto";
+import { toBillingCycleResponseDTO, toDomainBillingCycle } from "../mappers/billing-cycle-mapper";
 
 export class CreateBillingCycle {
     constructor(private billingCycleRepository: BillingCycleRepository) { }
 
-    async execute(input: CreateBillingCycleDTO) {
+    async execute(input: CreateBillingCycleDTO): Promise<BillingCycleResponseDTO> {
         const billingCycle = toDomainBillingCycle(input);
-        return await this.billingCycleRepository.create(billingCycle);
+        const newBillingCycle = await this.billingCycleRepository.create(billingCycle);
+        return toBillingCycleResponseDTO(newBillingCycle);
     }
 }
--- backend/src/application/use-cases/get-billing-cycle-by-id.ts
+++ backend/src/application/use-cases/get-billing-cycle-by-id.ts
@@ -1,10 +1,19 @@
+import { NotFoundError } from "../../domain/errors/not-found-error";
 import { BillingCycleRepository } from "../../domain/repositories/billing-cycle-repository";
 import { GetBillingCycleByIdDTO } from "../dtos/input/get-billing-cycle-by-id-dto";
+import { BillingCycleResponseDTO } from "../dtos/output/billing-cycle-response-dto";
+import { toBillingCycleResponseDTO } from "../mappers/billing-cycle-mapper";
 
 export class GetBillingCycleById {
     constructor(private billingCycleRepository: BillingCycleRepository) { }
 
-    async execute(input: GetBillingCycleByIdDTO) {
-        return await this.billingCycleRepository.findById(input.id);
+    async execute(input: GetBillingCycleByIdDTO): Promise<BillingCycleResponseDTO> {
+        const billingCycle = await this.billingCycleRepository.findById(input.id);
+
+        if (!billingCycle) {
+            throw new NotFoundError(`Billing cycle with id ${input.id} not found`);
+        }
+
+        return toBillingCycleResponseDTO(billingCycle);
     }
 }
