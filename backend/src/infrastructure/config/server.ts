f50c6fb5846b003a6d812cb41423588b9128605e
bzanf <borysfreitas@gmail.com>
2024-11-01 19:44:14 -0300

    Add express-async-errors dependency

--- backend/src/infrastructure/config/server.ts
+++ backend/src/infrastructure/config/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import 'express-async-errors';
 import router from "../../interface/routes/router";
 import { errorHandler } from "../../interface/middlewares/errorHandler";
 const app = express();
 
 app.use(express.json());
 app.use("/api", router);
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.SERVER_PORT || 3000;
 export async function startServer() {
