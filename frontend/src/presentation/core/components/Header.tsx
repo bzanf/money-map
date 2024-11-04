44903b196dfca19bf9fa92496fd1651ffc106d87
bzanf <borysfreitas@gmail.com>
2024-11-04 19:41:40 -0300

    Create header component

---
+++ frontend/src/presentation/core/components/Header.tsx
@@ -0,0 +1,17 @@
+export default () => (
+    <header className="main-header">
+        <a href="/#/" className="logo">
+            <span className="logo-mini">
+                MM
+            </span>
+            <span className="logo-lg">
+                <i className="fa fa-money"></i>
+                Money Map
+            </span>
+        </a>
+        <nav className="navbar navbar-static-top">
+            <a href="#" className="sidebar-toggle" data-toggle="offcanvas"></a>
+        </nav>
+    </header>
+
+)
