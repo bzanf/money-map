8c2d7a438b8beff5f60161179aa37c98d5c5b32c
bzanf <borysfreitas@gmail.com>
2024-11-05 21:50:53 -0300

    Create billing cycle and dashboard pages

---
+++ frontend/src/presentation/features/billing-cycle/pages/BillingCyclesPage.tsx
@@ -0,0 +1,7 @@
+const BillingCyclesPage = () => (
+    <div>
+        <h1>Billing cycles</h1>
+    </div>
+)
+
+export default BillingCyclesPage;
---
+++ frontend/src/presentation/features/dashboard/pages/DashboardPage.tsx
@@ -0,0 +1,7 @@
+const DashboardPage = () => (
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+)
+
+export default DashboardPage;
