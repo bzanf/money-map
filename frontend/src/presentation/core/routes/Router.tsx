c0e1185fe05b2abb3c85b43e04ab5d7ed6d10f75
bzanf <borysfreitas@gmail.com>
2024-11-07 18:18:16 -0300

    Fix 'BaseLayout' component import

--- frontend/src/presentation/core/routes/Router.tsx
+++ frontend/src/presentation/core/routes/Router.tsx
@@ -2,12 +2,12 @@
 import DashboardPage from "../../features/dashboard/pages/DashboardPage";
 import BillingCyclesPage from "../../features/billing-cycle/pages/BillingCyclesPage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Layout from "../pages/Layout";
+import BaseLayout from "../pages/BaseLayout";
 
 const AppRouter = () => (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path="/" element={<BaseLayout />}>
                 <Route index element={<DashboardPage />}></Route>
                 <Route path="/billing-cycles" element={<BillingCyclesPage />}></Route>
             </Route>
