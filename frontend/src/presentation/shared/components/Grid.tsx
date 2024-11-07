4ada5663558d7df61fa3ba85615ba26bf8374550
bzanf <borysfreitas@gmail.com>
2024-11-07 18:16:08 -0300

    Fix usage of multiple classes in 'Grid' component

--- frontend/src/presentation/shared/components/Grid.tsx
+++ frontend/src/presentation/shared/components/Grid.tsx
@@ -11,10 +11,10 @@ export default class Grid extends Component<GridProps> {
         const columns = numbers ? numbers.split(' ') : [];
         let classes = '';
 
-        if (columns[0]) classes += `col-xs-${columns[0]}`;
-        if (columns[1]) classes += `col-sm-${columns[1]}`;
-        if (columns[2]) classes += `col-md-${columns[2]}`;
-        if (columns[3]) classes += `col-lg-${columns[3]}`;
+        if (columns[0]) classes += `col-xs-${columns[0]} `;
+        if (columns[1]) classes += `col-sm-${columns[1]} `;
+        if (columns[2]) classes += `col-md-${columns[2]} `;
+        if (columns[3]) classes += `col-lg-${columns[3]} `;
 
         return classes;
     }
