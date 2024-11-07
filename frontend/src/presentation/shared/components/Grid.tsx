a15815d8fc966fd0a2367695df26fc7604ffcfd2
bzanf <borysfreitas@gmail.com>
2024-11-07 17:34:37 -0300

    Create 'Grid' and 'ValueBox' shared components

---
+++ frontend/src/presentation/shared/components/Grid.tsx
@@ -0,0 +1,29 @@
+import { Component, ReactNode } from "react";
+
+interface GridProps {
+    columns: string;
+    children?: ReactNode;
+}
+
+export default class Grid extends Component<GridProps> {
+
+    toCssClasses(numbers: string) {
+        const columns = numbers ? numbers.split(' ') : [];
+        let classes = '';
+
+        if (columns[0]) classes += `col-xs-${columns[0]}`;
+        if (columns[1]) classes += `col-sm-${columns[1]}`;
+        if (columns[2]) classes += `col-md-${columns[2]}`;
+        if (columns[3]) classes += `col-lg-${columns[3]}`;
+
+        return classes;
+    }
+
+    render() {
+        return (
+            <div className={this.toCssClasses(this.props.columns ?? '12')}>
+                {this.props.children}
+            </div>
+        );
+    };
+}
---
+++ frontend/src/presentation/shared/components/ValueBox.tsx
@@ -0,0 +1,25 @@
+import Grid from "./Grid"
+
+interface ValueBoxProps {
+    columns: string;
+    color: string;
+    icon: string;
+    value: number;
+    text: string;
+}
+
+const ValueBox = (props: ValueBoxProps) => (
+    <Grid columns={props.columns}>
+        <div className={`small-box bg-${props.color}`}>
+            <div className="inner">
+                <h3>{props.value}</h3>
+                <p>{props.text}</p>
+            </div>
+            <div className="icon">
+                <i className={`fa fa-${props.icon}`}></i>
+            </div>
+        </div>
+    </Grid>
+)
+
+export default ValueBox;
