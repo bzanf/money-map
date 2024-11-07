6b49f8f42263e9901d80dd68125628d6c900366e
bzanf <borysfreitas@gmail.com>
2024-11-07 18:19:14 -0300

    Change home link to use client side routing

--- frontend/src/presentation/core/components/Footer.tsx
+++ frontend/src/presentation/core/components/Footer.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => (
     <footer className="main-footer">
-        <strong>Copyright &copy; 2024 <a href="/#">Money Map</a>.</strong>
+        <strong>Copyright &copy; 2024 <Link to="/">Money Map</Link>.</strong>
         &nbsp;All rights reserved.
         <div className="float-right d-none d-sm-inline-block">
             <b>Version</b> 0.0.1
