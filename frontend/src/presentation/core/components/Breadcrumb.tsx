f67b9ace46e49c2b1ee3a5ba42ea5495105fdbd7
bzanf <borysfreitas@gmail.com>
2024-11-07 17:34:14 -0300

    Fix content header page text

--- frontend/src/presentation/core/components/Breadcrumb.tsx
+++ frontend/src/presentation/core/components/Breadcrumb.tsx
@@ -1,32 +1,41 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Breadcrumb = () => {
-    const location = useLocation();
-    const pathNames = location.pathname.split('/').filter(x => x);
-
-    return (
-        <ol className="breadcrumb float-sm-right">
-            <li className="breadcrumb-item">
-                <Link to="/">Home</Link>
-            </li>
-            {
-                pathNames.map((value, index) => {
-                    const to = `/${pathNames.slice(0, index + 1).join('/')}`;
-                    const isLast = index === pathNames.length - 1;
-
-                    return isLast ? (
-                        <li key={to} className="breadcrumb-item active" aria-current="page">
-                            {value.charAt(0).toUpperCase() + value.slice(1)}
-                        </li>
-                    ) : (
-                        <li key={to} className="breadcrumb-item">
-                            <Link to={to}>{value.charAt(0).toUpperCase() + value.slice(1)}</Link>
-                        </li>
-                    );
-                })
-            }
-        </ol>
-    );
+interface BreadcrumbProps {
+    pathNames: string[];
 }
+
+const Breadcrumb = (props: BreadcrumbProps) => (
+    <ol className="breadcrumb float-sm-right">
+        {
+            props.pathNames.length > 0
+                ? (
+                    <li className="breadcrumb-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                )
+                : (
+                    <li className="breadcrumb-item active">
+                        Home
+                    </li>
+                )
+        }
+        {
+            props.pathNames.map((value, index) => {
+                const to = `/${props.pathNames.slice(0, index + 1).join('/')}`;
+                const isLast = index === props.pathNames.length - 1;
+
+                return isLast ? (
+                    <li key={to} className="breadcrumb-item active" aria-current="page">
+                        {value.charAt(0).toUpperCase() + value.slice(1)}
+                    </li>
+                ) : (
+                    <li key={to} className="breadcrumb-item">
+                        <Link to={to}>{value.charAt(0).toUpperCase() + value.slice(1)}</Link>
+                    </li>
+                );
+            })
+        }
+    </ol>
+)
 
 export default Breadcrumb;
--- frontend/src/presentation/core/components/ContentHeader.tsx
+++ frontend/src/presentation/core/components/ContentHeader.tsx
@@ -1,22 +1,27 @@
+import { useLocation } from "react-router-dom";
 import Breadcrumb from "./Breadcrumb";
 
-const ContentHeader = () => (
-    <section className="content-header">
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-sm-6">
-                    <h1 className="m-0">Dashboard</h1>
-                </div>
-                <div className="col-sm-6">
-                    <Breadcrumb />
-                    {/* <ol className="breadcrumb float-sm-right">
-                        <li className="breadcrumb-item"><a href="#">Home</a></li>
-                        <li className="breadcrumb-item active">Dashboard</li>
-                    </ol> */}
+const ContentHeader = () => {
+    const location = useLocation();
+    const pathNames = location.pathname.split('/').filter(x => x);
+    const currentPage = pathNames.length > 0
+        ? pathNames[0].charAt(0).toUpperCase() + pathNames[0].slice(1)
+        : 'Home';
+
+    return (
+        <section className="content-header">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h1 className="m-0">{currentPage}</h1>
+                    </div>
+                    <div className="col-sm-6">
+                        <Breadcrumb pathNames={pathNames} />
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+}
 
 export default ContentHeader;
