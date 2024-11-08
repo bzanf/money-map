407349f311c48f4d217c3b68d8f6f40061ae4a5f
bzanf <borysfreitas@gmail.com>
2024-11-07 23:02:29 -0300

    Update 'DashboardPage': use app state values

--- frontend/src/presentation/features/dashboard/pages/DashboardPage.tsx
+++ frontend/src/presentation/features/dashboard/pages/DashboardPage.tsx
@@ -1,32 +1,39 @@
+import { RootState } from "../../../core/store/store";
 import Row from "../../../shared/components/Row";
 import ValueBox from "../../../shared/components/ValueBox";
+import { useSelector } from "react-redux";
 
-const DashboardPage = () => (
-    <Row>
-        <ValueBox
-            columns="12 4"
-            color="green"
-            icon="bank"
-            value="R$ 10"
-            text="Total credits"
-        />
+const DashboardPage = () => {
+    const { credit, debit } = useSelector((state: RootState) => state.dashboard.summary);
+    const netBalance = credit - debit;
 
-        <ValueBox
-            columns="12 4"
-            color="red"
-            icon="credit-card"
-            value="R$ 10"
-            text="Total debts"
-        />
+    return (
+        <Row>
+            <ValueBox
+                columns="12 4"
+                color="green"
+                icon="bank"
+                value={`R$ ${credit}`}
+                text="Total credits"
+            />
 
-        <ValueBox
-            columns="12 4"
-            color="blue"
-            icon="money-check-dollar"
-            value="R$ 0"
-            text="Net balance"
-        />
-    </Row>
-)
+            <ValueBox
+                columns="12 4"
+                color="red"
+                icon="credit-card"
+                value={`R$ ${debit}`}
+                text="Total debts"
+            />
+
+            <ValueBox
+                columns="12 4"
+                color="blue"
+                icon="money-check-dollar"
+                value={`R$ ${netBalance}`}
+                text="Net balance"
+            />
+        </Row>
+    )
+}
 
 export default DashboardPage;
