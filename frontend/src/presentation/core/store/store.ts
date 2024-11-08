a39ade984d35ea82619860a89fbde28164a5a273
bzanf <borysfreitas@gmail.com>
2024-11-08 18:13:39 -0300

    Create interfaces for state management

--- frontend/src/presentation/core/store/store.ts
+++ frontend/src/presentation/core/store/store.ts
@@ -7,6 +7,7 @@ export const store = configureStore({
     },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch'];
 export type AppThunk = ThunkAction<void, RootState, unknown, Action>;
