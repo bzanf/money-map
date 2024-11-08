89e8c11958c0492839e48bf63900866b7cc6316f
bzanf <borysfreitas@gmail.com>
2024-11-08 18:14:35 -0300

    Add return type

--- frontend/src/application/use-cases/get-billing-cycles-summary.ts
+++ frontend/src/application/use-cases/get-billing-cycles-summary.ts
@@ -3,7 +3,7 @@ import { BillingCycleRepository } from "../../domain/repositories/billing-cycle-
 export class GetBillingCyclesSummary {
     constructor(private billingCycleRepository: BillingCycleRepository) { }
 
-    async execute() {
+    async execute(): Promise<{ credit: number, debit: number }> {
         const summary = await this.billingCycleRepository.summary();
         return summary;
     }
