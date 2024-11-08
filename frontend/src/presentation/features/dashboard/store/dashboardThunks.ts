89212f869b2ad3dee47a9b102b330daa144e7494
bzanf <borysfreitas@gmail.com>
2024-11-08 18:14:08 -0300

    Add condition to avoid duplicate requests

--- frontend/src/presentation/features/dashboard/store/dashboardThunks.ts
+++ frontend/src/presentation/features/dashboard/store/dashboardThunks.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { DependencyContainer } from '../../../../infrastructure/di/dependency-container';
+import { selectSummaryStatus } from './dashboardSlice';
+import { RootState } from '../../../core/store/store';
 
 const useCase = DependencyContainer.getInstance().getBillingCyclesSummary();
 
@@ -7,6 +9,13 @@ export const fetchSummary = createAsyncThunk(
     'dashboard/summary',
     async () => {
         const data = await useCase.execute();
-        return data
+        return data;
+    },
+    {
+        condition(arg, thunkApi) {
+            const summaryStatus = selectSummaryStatus(thunkApi.getState() as RootState);
+            if (summaryStatus !== 'idle')
+                return false;
+        }
     }
 )
