d8c7bc28ab89dbeb60734e1537339e5cf516ff01
bzanf <borysfreitas@gmail.com>
2024-11-15 20:00:03 -0300

    Change repository implementation folder

--- frontend/src/infrastructure/http/repositories/http-billing-cycle-repository.ts
+++ frontend/src/infrastructure/http/repositories/http-billing-cycle-repository.ts
@@ -2,11 +2,19 @@ import axios from "axios";
 import { BillingCycle } from "../../../domain/entities/billing-cycle";
 import { BillingCycleRepository } from "../../../domain/repositories/billing-cycle-repository";
 import { API_URL } from "../../config/api-config";
+import { BillingCycleResponseDTO } from "../dtos/billing-cycle-response-dto";
+import { toDomainBillingCycle } from "../mappers/billing-cycle-mapper";
 
 export class HttpBillingCycleRepository implements BillingCycleRepository {
-    findAll(skip?: number, limit?: number): Promise<BillingCycle[]> {
-        throw new Error("Method not implemented.");
+
+    async findAll(params?: { skip?: number, limit?: number }): Promise<BillingCycle[]> {
+        const response = await axios.get<BillingCycleResponseDTO[]>(`${API_URL}/billing-cycles`);
+
+        return response.data
+            ? response.data.map(toDomainBillingCycle)
+            : [];
     }
+
     findById(id: string): Promise<BillingCycle | null> {
         throw new Error("Method not implemented.");
     }
@@ -22,6 +30,7 @@ export class HttpBillingCycleRepository implements BillingCycleRepository {
     count(): Promise<number> {
         throw new Error("Method not implemented.");
     }
+    
     async summary(): Promise<{ credit: number; debit: number; }> {
         const response = await axios.get<{ credit: number; debit: number; }>(`${API_URL}/billing-cycles/summary`);
         return response.data;
