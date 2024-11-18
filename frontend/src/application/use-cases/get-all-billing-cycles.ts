d37fea2e1a00d5491a41797708720e0c269dd934
bzanf <borysfreitas@gmail.com>
2024-11-18 20:22:32 -0300

    Set input parameter as not required

--- frontend/src/application/use-cases/get-all-billing-cycles.ts
+++ frontend/src/application/use-cases/get-all-billing-cycles.ts
@@ -1,10 +1,11 @@
+import { BillingCycle } from "../../domain/entities/billing-cycle";
 import { BillingCycleRepository } from "../../domain/repositories/billing-cycle-repository";
 import { GetAllBillingCyclesDTO } from "../dtos/input/get-all-billing-cycles-dto";
 
 export class GetAllBillingCycles {
     constructor(private billingCycleRepository: BillingCycleRepository) { }
 
-    async execute(input: GetAllBillingCyclesDTO) {
+    async execute(input?: GetAllBillingCyclesDTO): Promise<BillingCycle[]> {
         const billingCycles = await this.billingCycleRepository.findAll(input);
         return billingCycles;
     }
